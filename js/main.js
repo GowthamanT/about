2b7f120eda92e2a6fcf028603f2c796957676c15
Gowthaman T <happygowthaman@gmail.com>
2024-03-29 14:16:52 +0530

    feat: add versioning and display current version in UI - Added a package.json for versioning - Add a element in the website footer which displays the current version in the package.json

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ main();
  */
 function main() {
     setCurrentYearInCopyrightElm();
+    setCurrentVersionNumber();
     buildConsistencyGraph();
 }
 
@@ -15,6 +16,23 @@ function setCurrentYearInCopyrightElm() {
     const copyrightYearElement = document.getElementById('copyrightYear');
     const currentYear = new Date().getFullYear();
     copyrightYearElement.textContent = currentYear;
+}
+
+/**
+ * Sets the current version number in the package.json to the version element in the HTML.
+ */
+function setCurrentVersionNumber() {
+    fetch('package.json')
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById('version').innerText = data.version;
+        })
+        .catch((error) =>
+            console.error(
+                'Error in fetching the current version in package.json\n\n',
+                error
+            )
+        );
 }
 
 /**
