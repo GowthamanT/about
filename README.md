7f378a9bcfdbed41636e1f00d4517fcfb67cdc1c
Gowthaman T <happygowthaman@gmail.com>
2024-05-30 09:05:28 +0530

    feat: major design revamp

--- README.md
+++ README.md
@@ -1,31 +1,28 @@
 # Gowthaman T's personal portfolio
 
-This [portfolio](https://gowthamant.in/) is a short glimpse of me as a person. If you want to know what I do professionally, visit [G's Profession](https://gowthamant.github.io/work/).
+This [portfolio](https://gowthamant.in/) captures Little things about me.
 
 ## TL;DR
-- [About](#about)
-- [Blogs](#blogs)
-- [Technologies Used](#technologies-used)
-- [Contributing](#contributing)
-- [License](#license)
+
+-   [About](#about)
+-   [Technologies Used](#technologies-used)
+-   [Contributing](#contributing)
+-   [License](#license)
 
 ## About
-This portfolio is to portray me a person. Here you can find what I like, what are my values and get to know about what I do in life.
 
-## Blogs
-Mostly I write about my progress, tech or random musings on business and life.
+This portfolio is to capture things about me a person. Here you can find what I do, what I like and some extras!
 
 ## Technologies Used
-This portfolio website is built with minimal tech stack, no fancy things, basic HTML, CSS & JS. Git for version control and Github for hosting.
+
+This portfolio website is built with minimal tech stack. No fancy things - Basic HTML, CSS & JS. Git for version control and Github for hosting.
 
 ## Contributing
-As this is my personal website. I would love to create/update on my own. But if you wish to contribute, you can always point me any grammar errors, bugs, any enhancements,
+
+As this is my personal website. I would love to create/update on my own. But if you wish to contribute, you can always point me any errors, bugs, any enhancements,
 or things you want to know more about me. Please feel free to email me. My socials are available on my [portfolio](https://gowthamant.in/).
 
 ## License
-This is my personal work, If you want to take some inspiration, feel free to do that. But don't copy it on a whole. 
-This is not licensed for any use, especially for financial purposes. Please do not use this work without my permission.
 
----
-
-Feel free to connect with me on socials. I would to like to connect with like minded people!
+This is my personal work so if you want to take some inspiration, feel free to do that. But don't copy it on a whole.
+This is not licensed for any use, especially for financial purposes. Please do not use this work without my knowledge.
--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ main();
 function main() {
     setCurrentYearInCopyrightElm();
     setCurrentVersionNumber();
-    buildConsistencyGraph();
 }
 
 /**
@@ -33,101 +32,4 @@ function setCurrentVersionNumber() {
                 error
             )
         );
-}
-
-/**
- * A function that builds the consistency graph in the HTML.
- */
-function buildConsistencyGraph() {
-    /**
-     * TODO: Need to change this to get data from the API and load the table cells.
-     */
-    fetch('assets/habit-tracker-data.json')
-        .then((response) => response.json())
-        .then((data) => {
-            renderTableCells(data);
-        })
-        .catch((error) =>
-            console.error('Error in building consistency graph.\n\n', error)
-        );
-}
-
-/**
- * Render the data into the table cell.
- *
- * @param data - The habit tracker data.
- */
-function renderTableCells(data) {
-    data.forEach((item) => {
-        const weekday = getWeekday(item.date);
-        const row = document.getElementById(weekday);
-
-        const titleText = `Completed ${item.completedTasks}/${
-            item.outOff
-        } tasks on ${formatDisplayDate(item.displayData)}.`;
-
-        const tableCell = document.createElement('td');
-        tableCell.title = titleText;
-        tableCell.classList.add('graph-table-cell');
-
-        const graphCell = document.createElement('span');
-        graphCell.classList.add('graph-cell');
-        graphCell.classList.add(`level-${item.completedTasks}`);
-
-        tableCell.appendChild(graphCell);
-
-        row.appendChild(tableCell);
-    });
-}
-
-/**
- * Get the weekday from the given date string.
- *
- * @param dateString - A data string to which we need the weekday.
- * @returns the weekday for the particular date.
- */
-function getWeekday(dateString) {
-    const days = [
-        'sunday',
-        'monday',
-        'tuesday',
-        'wednesday',
-        'thursday',
-        'friday',
-        'saturday',
-    ];
-
-    const date = new Date(dateString);
-
-    return days[date.getDay()];
-}
-
-/**
- * Formats the date to display in the tooltip.
- *
- * @param date - A date string from data.
- * @returns the date to display in the tooltip.
- */
-function formatDisplayDate(dateString) {
-    const date = new Date(dateString);
-
-    const day = date.getDate();
-    const displayDate = date.toLocaleDateString('en-US', {
-        month: 'long',
-        day: 'numeric',
-    });
-
-    let suffix = '';
-
-    if (day === 1 || day === 21 || day === 31) {
-        suffix = 'st';
-    } else if (day === 2 || day === 22) {
-        suffix = 'nd';
-    } else if (day === 3 || day === 23) {
-        suffix = 'rd';
-    } else {
-        suffix = 'th';
-    }
-
-    return `${displayDate}${suffix}`;
 }
